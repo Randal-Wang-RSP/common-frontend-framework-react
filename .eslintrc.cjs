7b0c44345393f94640e870284bf54bb109eba094
Randal_Wang <randal_wang@resourcepro.com.cn>
2026-04-02 11:05:57 +0800

    fix(config): remove deprecated baseUrl and fix ESLint tsconfig project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: "./tsconfig.json",
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
     tsconfigRootDir: __dirname,
   },
   plugins: ["@typescript-eslint", "boundaries", "import"],
